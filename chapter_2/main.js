5b523417b4bd3f7b089c6656a212ccabf4f83655
Christian Awelakoue <awelakoue@gmail.com>
2019-08-01 14:01:52 +0200

    Variables, Constants & Comments

---
+++ chapter_2/main.js
@@ -0,0 +1,15 @@
+// jshint esversion:6
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100; // Assignment to constant variable
+// points = 45; 
+console.log(points);
+
+var score = 75; // ES5
+console.log(score);
